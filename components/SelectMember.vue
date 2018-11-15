b7759423edce3cdaf3477b20722bf0c6994f0cbd
minooo <13603983223@163.com>
2018-11-15 12:57:01 +0800

    base

---
+++ components/SelectMember.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="show"
+       class="s-box">
+    <p>默认初始值是{{show}}，所以是显示的</p>
+    <button @click.stop="closeDiv">关闭</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['show'],
+  methods: {
+    closeDiv() {
+      this.$emit('update:show', false) //触发 input 事件，并传入新值
+    }
+  }
+}
+</script>
+
+<style scoped>
+.s-box {
+  position: fixed;
+  width: 680px;
+  height: 457px;
+  background: red;
+}
+</style>
